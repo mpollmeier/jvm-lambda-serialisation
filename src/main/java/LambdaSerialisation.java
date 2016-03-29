0e34b9aa2a8e8962a5af2219c3c3a95cda241067
Michael Pollmeier <michael@michaelpollmeier.com>
2016-03-29 15:26:22 +1300

    cleanup

--- src/main/java/LambdaSerialisation.java
+++ src/main/java/LambdaSerialisation.java
@@ -2,23 +2,18 @@ import java.io.*;
 
 class LambdaSerialisation {
     public static void main(String[] args) throws Exception {
-        File file = new File("/tmp/lambda.ser");
+        File file = new File("lambda.ser");
 
         ObjectOutput oo = new ObjectOutputStream(new FileOutputStream(file));
         Runnable r = (Runnable & Serializable)() -> System.out.println("Serializable!");
         oo.writeObject(r);
         System.out.println("wrote lambda to " + file);
-
-        // ObjectInput oi = new ObjectInputStream(new FileInputStream(file));
-        // Runnable  r2 = (Runnable) oi.readObject();
-        // r2.run();
     }
 }
 
 class LambdaDeserialisation {
     public static void main(String[] args) throws Exception {
         File file = new File("lambda.ser");
-        // File file = new File("/tmp/lambda.ser");
         System.out.println("reading lambda from " + file);
 
         ObjectInput oi = new ObjectInputStream(new FileInputStream(file));
