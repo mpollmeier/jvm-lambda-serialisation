c5a4d0614bd4c55a197e05ca5c47f8d8a77c37ed
Michael Pollmeier <michael@michaelpollmeier.com>
2016-03-29 15:45:58 +1300

    serialise function (predicate)

--- src/main/java/LambdaSerialisation.java
+++ src/main/java/LambdaSerialisation.java
@@ -1,12 +1,22 @@
 import java.io.*;
+import java.util.function.*;
+
+class MyPredicate implements Predicate<Integer>, Serializable {
+    @Override
+    public boolean test(Integer i) {
+        return i == 42;
+    }
+}
 
 class LambdaSerialisation {
     public static void main(String[] args) throws Exception {
         File file = new File("lambda.ser");
 
         ObjectOutput oo = new ObjectOutputStream(new FileOutputStream(file));
-        Runnable r = (Runnable & Serializable)() -> System.out.println("Serializable!");
-        oo.writeObject(r);
+        Predicate<Integer> predicate = new MyPredicate();
+        // Runnable r = (Runnable & Serializable)() -> System.out.println("Serializable!");
+
+        oo.writeObject(predicate);
         System.out.println("wrote lambda to " + file);
     }
 }
@@ -17,7 +27,8 @@ class LambdaDeserialisation {
         System.out.println("reading lambda from " + file);
 
         ObjectInput oi = new ObjectInputStream(new FileInputStream(file));
-        Runnable  r = (Runnable) oi.readObject();
-        r.run();
+        Predicate<Integer> predicate = (Predicate<Integer>) oi.readObject();
+        System.out.println("predicate.test(42): " + predicate.test(42));
+        System.out.println("predicate.test(43): " + predicate.test(43));
     }
 }
